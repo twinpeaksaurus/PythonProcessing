e77071095d75b3fbce777a4a4396a1813cfe4dbe
twinpeaksaurus <jmogavero2@gmail.com>
2022-07-05 19:57:27 -0500

    task1 completed

--- Task1.py
+++ Task1.py
@@ -18,3 +18,25 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+#count up the number of rows in the first and second columns in each csv file
+#But what about repeats?
+#each time a new number is encountered, add it to a list of unique numbers
+#if the next number is already in there, do not count it
+
+unique_numbers = []
+
+for text in texts:
+    if text[0] not in unique_numbers:
+        unique_numbers.append(text[0])
+    if text[1] not in unique_numbers:
+        unique_numbers.append(text[1])
+
+for call in calls:
+    if call[0] not in unique_numbers:
+        unique_numbers.append(call[0])
+    if call[1] not in unique_numbers:
+        unique_numbers.append(call[1])
+
+
+print("There are " + len(unique_numbers) + " unique telephone numbers in the logs.")
